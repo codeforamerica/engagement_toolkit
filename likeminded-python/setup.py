0cbc5b3b13aa44c07c1748cb01596dbd7eac188b
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-04-13 11:51:19 -0700

    Add setup script for easy install.

---
+++ likeminded-python/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name="likeminded",
+    description="A wrapper for the LikeMinded REST API",
+    version="0.1",
+    packages=['likeminded','likeminded.utils','likeminded.utils.xml2dict'],
+    
+    author="Mjumbe Poe, Code for America",
+    author_email="mjumbe@codeforamerica.org",
+    
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        
+        "Operating System :: OS Independent",
+    ]
+)
