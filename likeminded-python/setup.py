8ea2d1968c1bcd86663379e128df9b4c54e9005c
Mjumbe Wawatu Poe <mjumbe@codeforamerica.org>
2011-04-13 16:28:48 -0700

    Move the requirements into setup.py

--- likeminded-python/setup.py
+++ likeminded-python/setup.py
@@ -3,8 +3,9 @@ from distutils.core import setup
 setup(
     name="likeminded",
     description="A wrapper for the LikeMinded REST API",
-    version="0.1",
+    version="0.2.0",
     packages=['likeminded','likeminded.utils','likeminded.utils.xml2dict'],
+    install_requires=['httplib2==0.6.0'],
     
     author="Mjumbe Poe, Code for America",
     author_email="mjumbe@codeforamerica.org",
