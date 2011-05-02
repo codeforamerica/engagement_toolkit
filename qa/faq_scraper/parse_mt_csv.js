cf1ff2722af755eeced7e4b2e5d88500392f2ce9
Aaron Ogle <atogle@gmail.com>
2011-05-02 16:13:10 -0700

    Support parsing a directory of files; make QA column reading more robust.

--- qa/faq_scraper/parse_mt_csv.js
+++ qa/faq_scraper/parse_mt_csv.js
@@ -1,29 +1,37 @@
 (function(){
-  var fs = require('fs');
+  var fs = require('fs'),
+      path = require('path');
 
   var parseLine = function(line, headers) {
     //NOTE: this leave extra double-quotes on the first and last value
     var values = line.split('","'),
-        url, date, 
+        url, date, id, match,
         parsedLines = [],
         data = {
           properties: {},
-          questions: [],
-          answers: []
+          questions: {},
+          answers: {}
         };
     
     headers.forEach(function(header, c) {
       val = strip(values[c]);
-      
+                
       if (val) {
-        if (header.lastIndexOf('A') === header.length-1) {
-          //If this is an answer, parse out the answer number from the key and 
-          //save the value
-          data.answers[parseInt(header.split('.')[1].replace('A', ''), 10)] = val;
-        } else if (header.lastIndexOf('Q') === header.length-1) {
-          //If this is an question, parse out the question number from the key 
-          //and save the value
-          data.questions[parseInt(header.split('.')[1].replace('Q', ''), 10)] = val;
+        match = /(\d+)/.exec(header);
+        
+        //Is this a Question or Answer?
+        if (header.indexOf('Answer') !== -1 && match) {
+          id = match[1];
+          
+          if (header.indexOf('Q') !== -1) {
+            //If this is an question, parse out the question number from the key 
+            //and save the value
+            data.questions[id] = val;
+          } else {
+            //If this is an answer, parse out the answer number from the key and 
+            //save the value
+            data.answers[id] = val;
+          } 
         } else {
           //These are other properties of the question
           data.properties[header] = val;        
@@ -33,15 +41,16 @@
     
     url = data.properties['Input.url'];
     date = data.properties['CreationTime'];
-    data.questions.forEach(function(question, qNum) {
+    
+    for (key in data.questions) {
       parsedLines.push({
         "url": url,
         "createDate": date,
-        "questionNumber": qNum,
-        "question": question,
-        "answer": data.answers[qNum]
+        "questionNumber": key,
+        "question": data.questions[key],
+        "answer": data.answers[key]
       });
-    });
+    }
     
     return parsedLines;
   };
@@ -94,16 +103,14 @@
     return str ? str.replace(/"\r*/, '').trim() : null;
   };
   
-  //Init
-  (function() {
-    var inputFile = process.argv[2],
-        fileStr,
+  var parseFile = function(inputFile, outputDir) {
+    var fileStr,
         headers,
         lines,
         parsedData = [],
         i;
-
-    if (inputFile) {
+    
+    if (inputFile) {      
       fileStr = fs.readFileSync(inputFile).toString();
       //Clean up all of the cells that contain line breaks
       fileStr = fileStr.replace(/([^"])(\r\n)+/g, '$1 ');
@@ -118,11 +125,31 @@
         parsedData = parsedData.concat(parseLine(lines[i], headers));
       }
             
-      //console.log(dedupedData);
-      console.log(toCsv(dedupe(parsedData)));
+      console.log(path.join(outputDir, path.basename(inputFile)));
+      fs.writeFile(path.join(outputDir, path.basename(inputFile)), toCsv(dedupe(parsedData)));
 
     } else {
       console.log('Usage: node parse_mt_csv.js turk_file_to_parse.csv');
     }
+  };
+  
+  //Init
+  (function() {
+    var input = process.argv[2],
+        outputDir = process.argv[3];
+    
+    path.exists(input, function (exists) {
+      if (exists) {
+        fs.readdir(input, function(errors, files) {
+          files.forEach(function(file) {            
+            if (file.indexOf('.csv') !== -1) {
+              parseFile(path.join(input, file), outputDir);
+            }
+          });
+        });
+      } else {
+        parseFile(input, outputDir);
+      }
+    });    
   })();
 })();
