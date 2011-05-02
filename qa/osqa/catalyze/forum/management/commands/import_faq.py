6215d455343fde671a2634d9126280cebed0a7b2
Tyler Stalder <tyler@stalder.me>
2011-05-02 14:35:29 -0700

    add dotcloud requirements file and change osqa import script

--- qa/osqa/catalyze/forum/management/commands/import_faq.py
+++ qa/osqa/catalyze/forum/management/commands/import_faq.py
@@ -6,15 +6,16 @@ import datetime
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         #retrieve the "asker" from the database
-        user = User.objects.get(id="1")
+        user = User.objects.get(id="9")
 
         #prepare question data
         qdata = dict(
-            title = "This is a sample imported question"
+            title = "This is a sample imported question",
             text = "I have stuff to do, just don't know how.",
-            tagnames = "test philly",
+            tags = "philadelphia",
         )
 
         #save the question, everything will be handled internally,
         #like creating the tags if they don't exist, etc 
-        AskAction(user=user).save(data=qdata)
+        question = AskAction(user=user).save(data=qdata)
+        print question.node
