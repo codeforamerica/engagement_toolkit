d92340a89f81cc33bcff11062a775241c01cda1a
John Mertens <john.mertens@gmail.com>
2011-05-02 16:27:13 -0700

    Refactor bing scraper

--- qa/faq_scraper/bing_scrape.rb
+++ qa/faq_scraper/bing_scrape.rb
@@ -10,17 +10,17 @@ class BingScraper
   # @params file path is the root path for the file
   # @params file name does not iunclude csv
   # @params query is a string for the actual query such as "Frequently Asked Questions"  
-  def initialize(domain, key, query, file_path, file_name)
+  def initialize(domain, key, file_path, file_name)
     @domain = domain
-    @query = query
+    @query = ''
     @file_name = file_name
     @file_path = file_path
     @bing = RBing.new(key)
-    scrape_bing
-    create_file
+    @data_store = []
   end
     
-  def scrape_bing
+  def scrape_bing(query)
+    @query = query
     @g = []
     a = 0
     # Only allows 1000 results, which is 20 pages * 50 results per page
@@ -38,20 +38,21 @@ class BingScraper
     a = 0
     # puts exclusion
     while a != 20
-      rsp = @bing.web('(site:'+@domain+') "Frequently Asked Questions"' + exclusion, {:count => 50, :offset => a})
+      rsp = @bing.web('(site:'+@domain+') "' + @query + '" ' + exclusion, {:count => 50, :offset => a})
       rsp["Web"]["Results"].each do |x|
         @g << x["Url"] unless @g.include?(x["Url"])
       end
       a = a + 1
       # puts a
     end
-
-
+    
+    @data_store = @data_store | @g
   end
 
   def create_file
-    CSV.open(@file_path + "/" + @file_name + ".csv", "a") do |csv|
-      @g.uniq.each do |x|
+    CSV.open(@file_path + "/" + @file_name + ".csv", "w") do |csv|
+      csv << ['url']
+      @data_store.uniq.each do |x|
         csv << [x]
       end
     end
--- qa/faq_scraper/bs_controller.rb
+++ qa/faq_scraper/bs_controller.rb
@@ -1,14 +1,19 @@
 require "./bing_scrape.rb"
 key = "248A1D8594169FE22B8F3D5BDB8F6C19141FD2F5"
 ts = Time.now.strftime("%Y%m%d_%H%M%S")
+path = "/Users/mertonium/Code/engagement_toolkit/qa/faq_scraper/bing_results"
 
 #domains = ['phila.gov','seattle.gov']
 terms = ['faq','frequently asked questions']
 
+phl = BingScraper.new('phila.gov',key,path,"PHL_#{ts}")
 terms.each do |t| 
-  BingScraper.new('phila.gov',key,t,"/Users/mertonium/Code/engagement_toolkit/qa/faq_scraper/bing_results","PHL_#{ts}")
+  phl.scrape_bing(t)
 end
+phl.create_file
 
+sea = BingScraper.new('seattle.gov',key,path,"SEA_#{ts}")
 terms.each do |t| 
-  BingScraper.new('seattle.gov', key,t,"/Users/mertonium/Code/engagement_toolkit/qa/faq_scraper/bing_results","SEA_#{ts}")
+  sea.scrape_bing(t)
 end
+sea.create_file
